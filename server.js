b48367598355a163e55540e18ae60fd863ff2966
cHAotiCK89 <149124445+cHAotiCK89@users.noreply.github.com>
2023-12-14 13:27:50 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,14 +14,7 @@ app.post('/', (req, res) => {
   const deviceInfo = req.body;
 
   // 情報をコンソールに出力
-  console.log('device info');
-  console.log(`OS: ${deviceInfo.os}`);
-  console.log(`browser: ${deviceInfo.browser}`);
-  console.log(`screen size: ${deviceInfo.screen}`);
-  console.log(`lang: ${deviceInfo.language}`);
   console.log(`location: ${deviceInfo.latitude}, ${deviceInfo.longitude}`);
-  console.log(`model: ${deviceInfo.model}`); 
-  console.log(`phone number: ${deviceInfo.phone}`);
 
   // IPアドレス
   const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
