c6e618ab51370e12d234e389304a10058b5e2866
cHAotiCK89 <149124445+cHAotiCK89@users.noreply.github.com>
2024-01-23 08:57:10 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-const cors = require('cors');
-app.use(cors());
-
-app.post('/', (req, res) => {
-
-  const deviceInfo = req.body;
-
-  console.log(`[location] : ${deviceInfo.latitude}, ${deviceInfo.longitude}`);  
-
-  const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  
-  console.log(`[ipaddress] : ${ipAddress}`);
-
-  res.send('Device information received.');
-
+app.get("/log-gps", (req, res) => {
+  // GPS情報の取得は同じ
+  res.send("Logged GPS");
 });
 
-app.listen(3000, () => {
-  console.log('Server is running!');
-});
+app.listen(3000);
