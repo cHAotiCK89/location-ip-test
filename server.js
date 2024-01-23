8ef361a651405ca11fed44599251523bca1da2dd
cHAotiCK89 <149124445+cHAotiCK89@users.noreply.github.com>
2024-01-23 19:54:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-const express = require('express');
-const app = express();
-
-app.get("/log-gps", (req, res) => {
-  const { lat, lng } = req.query; 
-  console.log(lat, lng); 
-  res.send("Logged GPS");
-});
-
-app.listen(3000);
+@app.route('/log', methods=['POST'])
+def log():
+  data = request.get_json()
+  print(data)
+  return "OK"
